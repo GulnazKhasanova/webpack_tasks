4b95b76c0616f329d835eaa552c7e1579cb78356
Gulnaz <viteewato@mail.ru>
2024-07-09 21:40:08 +0300

    new_webpack_4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import './index.css';
+
+import CAT_IMG from '/assets/image.png';
+
+const img = document.createElement('img');
+img.src = CAT_IMG;
+
+document.body.append(img);
+
 console.log('Hello World!');
